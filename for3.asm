d443c9259a33999391116f4285557082c971c044
sky line <32512284+linesky@users.noreply.github.com>
2022-08-11 10:27:17 +0100

    for loop sample

--- for3.asm
+++ for3.asm
@@ -1,8 +1,9 @@
 ;http://www.onlinepiccompiler.com/compiler16f84av1ENG.php
 var1 equ 0xc
+var2 equ 0xd
 
-
-
+movlw 0
+movwf var2
 movlw a
 movwf var1
 
@@ -10,5 +11,7 @@ movwf var1
 for
 decfsz var1,1
 goto for
+movlw ff
+movwf var2
 hlts
 goto hlts
