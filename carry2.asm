e41dba7cdae5203f9d9ee503aee2f43239dc2b0d
sky line <32512284+linesky@users.noreply.github.com>
2022-08-11 09:23:43 +0100

    counter 4 bytes

---
+++ carry2.asm
@@ -0,0 +1,26 @@
+;http://www.onlinepiccompiler.com/compiler16f84av1ENG.php
+var1 equ 0xc
+var2 equ 0xd
+var3 equ 0xe
+var4 equ 0xf
+
+
+movlw 0
+movwf var4
+movlw ff
+movwf var3
+movlw f8
+movwf var1
+movlw ff
+movwf var2
+
+again
+incfsz var1,1
+goto again
+incfsz var2,1
+goto again
+incfsz var3,1
+goto again
+incfsz var4,1
+goto again
+goto again
